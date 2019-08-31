bfc9a644de1673baa8ba235d2d5636a52f91cde8
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-08-31 19:24:01 +0530

    controller: main program to run at-controller

--- main.go
+++ main.go
@@ -4,8 +4,17 @@ import (
 	"flag"
 	"os"
 	"path/filepath"
+	"time"
 
+	"k8s.io/apimachinery/pkg/util/wait"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog"
+
+	clientset "github.com/mfrw/controller/pkg/generated/clientset/versioned"
+	informers "github.com/mfrw/controller/pkg/generated/informers/externalversions"
 )
 
 var (
@@ -20,6 +29,37 @@ func main() {
 	klog.InitFlags(nil)
 
 	flag.Parse()
+
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		cfg, err = clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+		if err != nil {
+			klog.Fatalf("Error building kubeconfig: %s", err.Error())
+		}
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	cnatClient, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building cnat clientset: %s", err.Error())
+	}
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Minute*10)
+	cnatInformerFactory := informers.NewSharedInformerFactory(cnatClient, time.Minute*10)
+
+	controller := NewController(kubeClient, cnatClient, cnatInformerFactory.Cnat().V1alpha1().Ats(), kubeInformerFactory.Core().V1().Pods())
+
+	kubeInformerFactory.Start(wait.NeverStop)
+	cnatInformerFactory.Start(wait.NeverStop)
+
+	if err := controller.Run(2, wait.NeverStop); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
+
 }
 
 func defaultKubeconfig() string {
