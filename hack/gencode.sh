09d4fcacb5b6ccc59e0a80558792c5125f84e9c7
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-08-31 18:32:05 +0530

    hack: format codegen cmdline

--- hack/gencode.sh
+++ hack/gencode.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-$HOME/g/src/k8s.io/code-generator/generate-groups.sh "all" github.com/mfrw/controller/pkg/generated github.com/mfrw/controller/pkg/apis cnat:v1alpha1
+$HOME/g/src/k8s.io/code-generator/generate-groups.sh "all" \
+	github.com/mfrw/controller/pkg/generated \
+	github.com/mfrw/controller/pkg/apis \
+	cnat:v1alpha1
