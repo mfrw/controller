cbf5b12a18e6dfd62b5ba8d0ed5b512cc843b43c
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-08-31 18:34:05 +0530

    pkg: cnat: register: add comments

--- pkg/apis/cnat/v1alpha1/register.go
+++ pkg/apis/cnat/v1alpha1/register.go
@@ -8,12 +8,15 @@ import (
 	"github.com/mfrw/controller/pkg/apis/cnat"
 )
 
+// SchemeGroupVersion is group version used to register these objects
 var SchemeGroupVersion = schema.GroupVersion{Group: cnat.GroupName, Version: "v1alpha1"}
 
+// Kind takes an unqualified kind and returns back a Group qualified GroupKind
 func Kind(kind string) schema.GroupKind {
 	return SchemeGroupVersion.WithKind(kind).GroupKind()
 }
 
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
 func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
@@ -23,6 +26,7 @@ var (
 	AddToScheme   = SchemeBuilder.AddToScheme
 )
 
+// Adds the list of known types to Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&At{},
